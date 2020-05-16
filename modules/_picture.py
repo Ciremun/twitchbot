b2fc181afc7d9d83d30c8208b09a6173740477e4
Ciremun <Ciremun@yandex.ru>
2020-05-16 18:03:07 +0300

    handle PIL.UnidentifiedImageError exception

--- modules/_picture.py
+++ modules/_picture.py
@@ -5,7 +5,7 @@ import _globals as g
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 
 class FlaskImageApp(threading.Thread):
@@ -32,7 +32,11 @@ class FlaskImageApp(threading.Thread):
         self.socketio.run(self.app)
 
     def set_image(self, folder, filename):
-        img = Image.open(f'data/{folder}{filename}')
+        try:
+            img = Image.open(f'data/{folder}{filename}')
+        except UnidentifiedImageError:
+            print(f'UnidentifiedImageError - data/{folder}{filename}')
+            return
         ri, rs = img.width / img.height, g.screenwidth / g.screenheight
         width, height = u.resizeimg(ri, rs, img.width, img.height, g.screenwidth, g.screenheight)
         self.socketio.emit('setimage', {'width': width, 'height': height, 'src': f'{folder}{filename}'})
