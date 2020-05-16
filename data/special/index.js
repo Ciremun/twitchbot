aaac7624b4076218d75a2c37956941630f921232
Ciremun <Ciremun@yandex.ru>
2020-05-16 13:59:19 +0300

    init

---
+++ data/special/index.js
@@ -0,0 +1,22 @@
+var socket = io();
+
+socket.on('connect_', function(data) {
+    var css = 'body { width: ' + data['screenwidth'] + 'px; height: ' + data['screenheight'] + 'px; margin: 0; } ' + 
+                'img { max-width: ' + data['screenwidth'] + 'px; max-height: ' + data['screenheight'] + 'px; } ' + 
+                'div.outer { position: relative; height: ' + data['screenheight'] + 'px; } ' +
+                'div.outer img { position: absolute; right: 0px; bottom: 0px; }',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+    head.appendChild(style);
+    style.appendChild(document.createTextNode(css));
+});
+
+function set_image(width, height, src) {
+    var img = document.getElementById('img');
+    var new_img = new Image();
+    new_img.id = 'img';
+    new_img.style = 'width: ' + width + 'px; height: ' + height + 'px;';
+    new_img.src = src;
+    document.getElementById('imgdiv').appendChild(new_img);
+    img.parentNode.removeChild(img);
+}
